e06af2a87ec10a95724eac86b56a0ac52ed1e832
incar0615 <incar0615@gmail.com>
2017-06-13 06:14:15 +0900

    Add files via upload

--- Source/JCH/MazeGenerator.h
+++ Source/JCH/MazeGenerator.h
@@ -5,7 +5,19 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "MazeWall.h"
+#include "MazeBlock.h"
+#include "MazeDoor.h"
 #include "MazeGenerator.generated.h"
+
+
+enum TileState
+{
+	EMPTY = 0, WALL, ROOM , CORRIDOR, DOOR,
+};
+
+enum Direction {
+	EAST = 0 , WEST , NORTH , SOUTH
+};
 
 UCLASS()
 class JCH_API AMazeGenerator : public AActor
@@ -20,9 +32,24 @@ public:
 		class UStaticMeshComponent* rCompo;
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Map")
+		int maxRoom = 6;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Map")
 		int MapSize;
-
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Map")
+		int MaxMapSize;
 	
+	
+	UPROPERTY()
+		TArray<int> wallTilelist;
+	
+		//TileState* tileMap = new TileState[MapSize * MapsSize];
+		TileState tileMap[250][250];
+		
+		int roomCnt;
+	void GenerateDungeon();
+	void GenerateDungeon2();
+	void MakeRoom(int _tileNum);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
